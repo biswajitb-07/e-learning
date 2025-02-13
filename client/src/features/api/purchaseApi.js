64c55657746760474ab940fa270db78d3400c221
codebybiswa <barikbiswajit152@gmail.com>
2025-02-13 16:32:23 +0530

    api changes

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "https://e-learning-sryx.onrender.com/api/v1/purchase";
+const COURSE_PURCHASE_API =
+  "https://e-learning-sryx.onrender.com/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
+  tagTypes: ["Refetch_Course_Details"],
   baseQuery: fetchBaseQuery({
     baseUrl: COURSE_PURCHASE_API,
     credentials: "include",
@@ -16,6 +18,7 @@ export const purchaseApi = createApi({
         body: { courseId },
         headers: { "Content-Type": "application/json" },
       }),
+      invalidatesTags: ["Refetch_Course_Details"],
     }),
     confirmPayment: builder.mutation({
       query: ({ orderId, paymentId, userId, courseId, amount }) => ({
@@ -24,12 +27,14 @@ export const purchaseApi = createApi({
         body: { orderId, paymentId, userId, courseId, amount },
         headers: { "Content-Type": "application/json" },
       }),
+      invalidatesTags: ["Refetch_Course_Details"],
     }),
     getCourseDetailWithStatus: builder.query({
       query: (courseId) => ({
         url: `/course/${courseId}/detail-with-status`,
         method: "GET",
       }),
+      providesTags: ["Refetch_Course_Details"],
     }),
     getPurchasedCourses: builder.query({
       query: () => ({
